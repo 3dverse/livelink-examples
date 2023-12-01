0307bcfa9046f0492374a519ceffb2060ba44aa3
Sébastien PERIN <sebastien@vizua3d.com>
2023-12-01 19:16:43 +0100

    feat: a better spawn entities howto (including debug lines to see lights)

--- how-to-spawn-entities/main.js
+++ how-to-spawn-entities/main.js
@@ -1,88 +1,146 @@
 window.addEventListener('load', initApp);
 
 // The scene that is spawned when clicking "Link Scene"
-const sceneToLinkUUID = '49c0a2e9-d170-4d90-b757-5ccbaea12f84';
+const sceneToLinkUUID = 'LINK_SCENE_UUID';
 let linkedSceneEntity = null;
 let lightEntity = null;
+let btnToggleScene;
+let btnToggleLight;
 
 async function initApp() {
-    await SDK3DVerse.joinOrStartSession({
-        userToken: 'public__Stxwks57CH39bjP',
-        sceneUUID: '30c5e730-129f-4749-a87f-e2b3fd76a209',
+    btnToggleScene = document.getElementById("btn-toggle-scene");
+    btnToggleLight = document.getElementById("btn-toggle-light");
+    btnToggleScene.innerText = "Spawn Scene";
+    btnToggleLight.innerText = "Spawn Light";
+
+    const isSessionCreator = await SDK3DVerse.joinOrStartSession({
+        userToken: 'PUBLIC_TOKEN',
+        sceneUUID: 'SCENE_UUID',
         canvas: document.getElementById('display-canvas'),
         viewportProperties: {
             defaultControllerType: SDK3DVerse.controller_type.orbit,
         },
     });
 
-    // Set camera settings to make the light entity more visible
+    // Set camera settings to make the light entities more visible
     const cameraEntity = SDK3DVerse.engineAPI.cameraAPI.getActiveViewports()[0].getCamera();
     cameraEntity.setComponent('camera', {
         dataJSON: {
             // Disable reflection
             reflection: false,
-            environment: {
-                // Use gradient as environment
-                skybox: false,
-                atmosphere:false,
-                gradient:true
-            }
+            debugLines: true,
+            skybox: false,
+            atmosphere: false,
+            gradient: true,
         }
     });
 
     // Set bottom gradient to black (top is white)
     SDK3DVerse.engineAPI.propagateSceneSettings({ environment: { ambientColorBottom: [0, 0, 0] } });
 
-    // Spawn the light otherwise the cube is dark
-    spawnLight();
+    // Spawn a light otherwise the cube is dark due to its 100% metalness.
+    // But do it a single time for the whole session life time.
+    if(isSessionCreator) {
+        const pointLightComponentValue = {
+            color: [1, 1, 1],
+            intensity: 500,
+            range: 0,
+            isDirectional: false,
+            isSun: false
+        };
+        const transform = {
+            position: [3, 3, 3],
+            orientation: SDK3DVerse.utils.quaternionFromEuler([-30, 45, 0])
+        };
+        const options = {
+            deleteOnClientDisconnection: false,
+            cutoff: 80
+        };
+        spawnLight(
+            pointLightComponentValue,
+            transform,
+            options
+        );
+    }
 }
 
-window.spawnSceneLinker = async function() {
-    if(linkedSceneEntity) {
-        return;
-    }
+const spawnSceneLinker = async function(sceneTransform, options = {}) {
+    const {
+        parentEntity = null,
+        // delete entity if the client disconnects
+        deleteOnClientDisconnection = true
+    } = options;
 
     let template = new SDK3DVerse.EntityTemplate();
     template.attachComponent('scene_ref', { value: sceneToLinkUUID });
-    template.entityTemplate.local_transform.position = [0,0,-3];
-    const deleteOnClientDisconnection = true;
-    const parentEntity = null;
-    linkedSceneEntity = await template.instantiateTransientEntity("linked scene name", parentEntity, deleteOnClientDisconnection);
+    template.attachComponent('local_transform', {
+        ...template.entityTemplate.local_transform,
+        ...sceneTransform
+    });
+    const entity = await template.instantiateTransientEntity("linked scene name", parentEntity, deleteOnClientDisconnection);
+    return entity;
 }
 
-window.deleteSceneLinker = async function() {
-    if(!linkedSceneEntity) {
-        return;
+window.toggleSceneLinker = async function() {
+    if(linkedSceneEntity) {
+        await SDK3DVerse.engineAPI.deleteEntities([linkedSceneEntity]).finally(() => {
+            linkedSceneEntity = null;
+            btnToggleScene.innerText = "Spawn Scene";
+        });
     }
-
-    await SDK3DVerse.engineAPI.deleteEntities([linkedSceneEntity]);
-    linkedSceneEntity = null;
+    else {
+        linkedSceneEntity = await spawnSceneLinker({ position: [0, 0, -3] });
+        btnToggleScene.innerText = "Delete Scene";
+    }
 }
 
-window.spawnLight = async function() {
-    if(lightEntity) {
-        return;
-    }
+const spawnLight = async function(pointLightComponentValue, pointLightTransform, options = {}) {
+    const {
+        parentEntity = null,
+        // delete entity if the client disconnects
+        deleteOnClientDisconnection = true,
+        // the cut off of the spot light
+        cutoff = 50
+     } = options;
 
     let template = new SDK3DVerse.EntityTemplate();
-    template.attachComponent('point_light', {
-        color: [1, 0, 0],
-        intensity: 50,
-        range: 0,
-        isDirectional: false,
-        isSun: false
+    template.attachComponent('point_light', pointLightComponentValue);
+    template.attachComponent('spot_light', { cutoff });
+    template.attachComponent('local_transform', {
+        ...template.entityTemplate.local_transform,
+        ...pointLightTransform
     });
-    template.entityTemplate.local_transform.position = [1.5, 1.5, 0.75];
-    const deleteOnClientDisconnection = true;
-    const parentEntity = linkedSceneEntity;
-    lightEntity = await template.instantiateTransientEntity("light name", parentEntity, deleteOnClientDisconnection);
+    const entity = await template.instantiateTransientEntity("light name", parentEntity, deleteOnClientDisconnection);
+    return entity;
 }
 
-window.deleteLight = async function() {
-    if(!lightEntity) {
-        return;
+window.toggleLight = async function() {
+    if(lightEntity) {
+        await SDK3DVerse.engineAPI.deleteEntities([lightEntity]).finally(() => {
+            lightEntity = null;
+            btnToggleLight.innerText = "Spawn Light";
+        });
     }
-
-    await SDK3DVerse.engineAPI.deleteEntities([lightEntity]);
-    lightEntity = null;
+    else {
+        const pointLightComponentValue = {
+            color: [1, 0, 0],
+            intensity: 300,
+            range: 0,
+            isDirectional: false,
+            isSun: false
+        };
+        const transform = {
+            position: linkedSceneEntity ? [0, 2, 2] : [0, 2, 1],
+            orientation: SDK3DVerse.utils.quaternionFromEuler([-45, 0, 0])
+        };
+        const options = {
+            parentEntity: linkedSceneEntity
+        };
+        lightEntity = await spawnLight(
+            pointLightComponentValue,
+            transform,
+            options
+        );
+        btnToggleLight.innerText = "Delete Light";
+    }
 }
